e44612ce4da0947ba408a90aac2c1cc4bdb6ee3b
S00ahKim <rlatndk0206@naver.com>
2019-06-08 02:37:27 +0900

    삭제 다이얼로그 수정

--- screens/DeleteProfileScreen.js
+++ screens/DeleteProfileScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, ToastAndroid } from "react-native";
 import Dialog from "react-native-dialog";
 
-export default class EditProfileScreen extends React.Component {
+export default class DeleteProfileScreen extends React.Component {
   state = {
     dialogVisible: true,
   }
@@ -13,13 +13,14 @@ export default class EditProfileScreen extends React.Component {
  
   handleCancel = () => {
     this.setState({ dialogVisible: false });
-    goBack()
+    ToastAndroid.show('취소되었습니다', ToastAndroid.SHORT);
   };
  
   handleDelete = () => {
     // The user has pressed the "Delete" button, so here you can do your own logic.
     // ...Your logic
     this.setState({ dialogVisible: false });
+    ToastAndroid.show('이용해 주셔서 감사합니다.', ToastAndroid.SHORT);
   };
 
   render() {
@@ -27,12 +28,12 @@ export default class EditProfileScreen extends React.Component {
     return (
       <View>
         <Dialog.Container visible={this.state.dialogVisible}>
-          <Dialog.Title> 계정 삭제 </Dialog.Title>
+          <Dialog.Title>계정 삭제</Dialog.Title>
           <Dialog.Description>
-            정말 계정을 삭제하시겠습니까?
+            정말 계정을 삭제하시겠습니까? 계정을 삭제하시면 되돌릴 수 없습니다.
           </Dialog.Description>
-          <Dialog.Button label="취소" onPress={this.handleCancel}/>
-          <Dialog.Button label="삭제" onPress={this.handleDelete}/>
+          <Dialog.Button label="취소" onPress={this.handleCancel} />
+          <Dialog.Button label="삭제" onPress={this.handleDelete} />
         </Dialog.Container>
       </View>
     );
